0ce3411364dc1fd3d04f7402668695186e285fe7
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2017-05-08 17:05:19 -0400

    update howto section

--- _downloads/params_ica.py
+++ _downloads/params_ica.py
@@ -1,13 +1,8 @@
 """ Parameters file for run_preprocess_pipeline.py """
 
-test = True
-
 data_type = 'ds'
 
 main_path = '/run/media/pasca/paska/meg_data/omega/sample_BIDS_omega/'
-if test:
-    data_type = 'fif'
-
 data_path = main_path
 
 subject_ids = ['sub-0002', 'sub-0003', 'sub-0004', 'sub-0006', 'sub-0007']
@@ -30,13 +25,4 @@ variance = 0.95
 reject = dict(mag=5e-12, grad=5000e-13)
 
 # pipeline directory within the main_path dir
-preproc_pipeline_name = "preprocessing_pipeline"
-if test:
-    preproc_pipeline_name = preproc_pipeline_name + '_test'
-
-    is_set_ICA_components = False
-
-    if is_set_ICA_components:
-        n_comp_exclude = {'sub-0002': {'ses-0001': [0, 1, 3]}}
-    else:
-        n_comp_exclude = []
+preproc_pipeline_name = 'preprocessing_pipeline'
