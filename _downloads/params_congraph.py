f8c83e81b82b851cb6bdbf232559ba0c656e43d1
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2017-05-09 23:28:28 -0400

    added src section

--- _downloads/params_congraph.py
+++ _downloads/params_congraph.py
@@ -11,8 +11,12 @@ sessions = ['ses-0001']
 
 # ----------------------- SET connectivity variables ----------------------#
 
-freq_bands = [[2, 4], [5, 7], [8, 12], [13, 29], [30, 59], [60, 90]]
-freq_band_names = ['delta', 'theta', 'alpha', 'beta', 'gamma1', 'gamma2']
+# freq_bands = [[2, 4], [5, 7], [8, 12], [13, 29], [30, 59], [60, 90]]
+# freq_band_names = ['delta', 'theta', 'alpha', 'beta', 'gamma1', 'gamma2']
+
+freq_bands = [[8, 12], [13, 29]]
+freq_band_names = ['alpha', 'beta']
+
 
 # 'pli', 'plv', 'pli2_unbiased', 'coh', 'cohy', 'ppc', 'wpli'
 # 'imcoh', 'wpli2_debiased', 'correl'
